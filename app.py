e146df3a56824f33f9b79b8755996f93cfd9c111
DreamWaker26 <dw263026@gmail.com>
2018-03-19 22:27:21 +0800

    Function

--- app.py
+++ app.py
@@ -56,6 +56,9 @@ def index():
 
 	return render_template('index.html', posts=posts)
 
+@app.route("/<path:filename>")
+def template(filename):
+	return render_template(filename)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -126,61 +129,32 @@ def post(post_id):
 
     return render_template('post.html', post=post)
 
-@app.route('/add')
-def add():
-    return render_template('add.html')
+
+
 
 @app.route('/addpost', methods=['POST'])
 def addpost():
-    title = request.form['title']
-    keyword = request.form['keyword']
-    author = request.form['author']
-    content = request.form['content']
-    picture = request.form['picture']
-    print (picture)
 
-    post = Blogpost(title=title, picture=picture, keyword=keyword, author=author, content=content, date_posted=datetime.now())
-	
-    #db.session.add(post)
-    #db.session.commit()
-	
-    return redirect(url_for('index'))
-""
-@app.route('/upload', methods=['GET', 'POST'])
-def upload_file():
-    if request.method == 'POST':
-        # check if the post request has the file part
-        """if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
-        
-        # if user does not select file, browser also
-        # submit a empty part without filename
-        if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
-        """
-        picture = request.files['picture']
-        if file and allowed_file(file.filename):
-            #filename = secure_filename(file.filename)
-            filename = file.filename
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return Blogpost(picture=file.filename)
+	if request.method == "POST":
 
+	    title = request.form['title']
+	    keyword = request.form['keyword']
+	    author = request.form['author']
+	    content = request.form['content']
+	    picture = request.files['picture']
+	    
+	    if picture and allowed_file(picture.filename):
+	            #filename = secure_filename(file.filename)
+	            filename = picture.filename
+	            picture.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
 
+	    post = Blogpost(title=title, picture=picture.filename, keyword=keyword, author=author, content=content, date_posted=datetime.now())
+		
+	    db.session.add(post)
+	    db.session.commit()
+		
+	    return redirect(request.referrer)
 
-    return redirect(url_for('index'))
-    """
-    return '''
-    <!doctype html>
-    <title>Upload new File</title>
-    <h1>Upload new File</h1>
-    <form method=post enctype=multipart/form-data>
-      <p><input type=file name=file>
-         <input type=submit value=Upload>
-    </form>
-    '''
-"""
 
 
 
